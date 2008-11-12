b13a5d35c75d85735ca1b5252dffd3c2341e9521
merlinofchaos <merlinofchaos>
2008-11-12 04:24:26 +0000

    Some minor documentation updates that I clearly missed.

--- delegator/js/task-handlers.js
+++ delegator/js/task-handlers.js
@@ -1,8 +1,14 @@
-// $Id $
+// $Id$
 
 Drupal.behaviors.zzGoLastDelegatorTaskList = function(context) {
   var id = 'delegator-task-list-arrange';
 
+  /**
+   * There's no way from PHP to tell the tabledrag code to turn on the
+   * 'table has changed' code. So what we do is look for a particular
+   * class and if so, invoke it. We also look to see if we should
+   * hilite a particular row as the 'previous' drag.
+   */
   if ($('#' + id, context).hasClass('changed') && Drupal.tableDrag[id]) {
     Drupal.tableDrag[id].changed = true;
     $(Drupal.theme('tableDragChangedWarning')).insertAfter(Drupal.tableDrag[id].table);
