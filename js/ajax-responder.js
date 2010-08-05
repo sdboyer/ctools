6ca82780c913b7a89d8fe07960265bb2b4f0c854
merlinofchaos <merlinofchaos>
2010-08-05 19:03:12 +0000

    #718368: Ignore query string when testing js and css files for inclusion during AJAX requests.

--- js/ajax-responder.js
+++ js/ajax-responder.js
@@ -359,14 +359,27 @@
     // Build a list of css files already loaded:
     $('link:not(.ctools-temporary-css)').each(function () {
       if ($(this).attr('type') == 'text/css') {
-        Drupal.CTools.AJAX.css[$(this).attr('href')] = $(this).attr('href');
+        var link = $(this).attr('href');
+        if (link) {
+          // Strip off the query string; this can change at times and cause us
+          // to include files multiple times.
+          var index = link.indexOf('?');
+          if (index) {
+            link = link.substr(0, index);
+          }
+          Drupal.CTools.AJAX.css[link] = $(this).attr('href');
+        }
       }
     });
 
     var html = '';
     for (i in data.argument) {
-      if (!Drupal.CTools.AJAX.css[data.argument[i].file]) {
-//        Drupal.CTools.AJAX.css[data.argument[i].file] = data.argument[i].file;
+      var link = data.argument[i].file;
+      var index = link.indexOf('?');
+      if (index) {
+        link = link.substr(0, index);
+      }
+      if (!Drupal.CTools.AJAX.css[link]) {
         html += '<link class="ctools-temporary-css" type="text/css" rel="stylesheet" media="' + data.argument[i].media +
           '" href="' + data.argument[i].file + '" />';
       }
@@ -386,14 +399,29 @@
     // Build a list of scripts already loaded:
     var scripts = {};
     $('script').each(function () {
-      Drupal.CTools.AJAX.scripts[$(this).attr('src')] = $(this).attr('src');
+      var link = $(this).attr('src');
+      if (link) {
+          // Strip off the query string; this can change at times and cause us
+          // to include files multiple times.
+        var index = link.indexOf('?');
+        if (index) {
+          link = link.substr(0, index);
+        }
+        Drupal.CTools.AJAX.scripts[link] = $(this).attr('src');
+      }
     });
 
     var html = '';
     var head = document.getElementsByTagName('head')[0];
     for (i in data.argument) {
-      if (!Drupal.CTools.AJAX.scripts[data.argument[i]]) {
-        Drupal.CTools.AJAX.scripts[data.argument[i]] = data.argument[i];
+      var link = data.argument[i];
+      var index = link.indexOf('?');
+      if (index) {
+        link = link.substr(0, index);
+      }
+
+      if (!Drupal.CTools.AJAX.scripts[link]) {
+        Drupal.CTools.AJAX.scripts[data.argument[i]] = link;
         // Use this to actually get the script tag into the dom, which is
         // needed for scripts that self-reference to determine paths.
         var script = document.createElement('script');
