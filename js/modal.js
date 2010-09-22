576aadc71ce3ff976b6cafa74482b8e075fe0f06
Earl Miles <merlin@logrus.com>
2010-09-22 16:30:47 -0700

    Fix modal double submit.

--- js/modal.js
+++ js/modal.js
@@ -186,7 +186,7 @@
   /**
    * Bind links that will open modals to the appropriate function.
    */
-  Drupal.behaviors.ZZCToolsModal = { 
+  Drupal.behaviors.ZZCToolsModal = {
     attach: function(context) {
       // Bind links
       // Note that doing so in this order means that the two classes can be
@@ -204,7 +204,7 @@
       $('a.ctools-use-modal:not(.ctools-use-modal-processed)', context)
         .addClass('ctools-use-modal-processed')
         .click(Drupal.CTools.Modal.clickAjaxLink)
-        .each(function () { 
+        .each(function () {
           // Create a drupal ajax object
           var element_settings = {};
           if ($(this).attr('href')) {
@@ -214,7 +214,7 @@
           var base = $(this).attr('href');
           Drupal.ajax[base] = new Drupal.ajax(base, this, element_settings);
 
-          // Attach the display behavior to the ajax object 
+          // Attach the display behavior to the ajax object
           Drupal.ajax[base].commands.modal_display = Drupal.CTools.Modal.modal_display;
           Drupal.ajax[base].commands.modal_dismiss = Drupal.CTools.Modal.modal_dismiss;
         }
@@ -224,7 +224,7 @@
       $('input.ctools-use-modal:not(.ctools-use-modal-processed), button.ctools-use-modal:not(.ctools-use-modal-processed)', context)
         .addClass('ctools-use-modal-processed')
         .click(Drupal.CTools.Modal.clickAjaxLink)
-        .each(function() { 
+        .each(function() {
           var element_settings = {};
 
           // AJAX submits specified in this manner automatically submit to the
@@ -235,23 +235,23 @@
           var base = $(this).attr('id');
           Drupal.ajax[base] = new Drupal.ajax(base, this, element_settings);
 
-          // Attach the display behavior to the ajax object 
+          // Attach the display behavior to the ajax object
           Drupal.ajax[base].commands.modal_display = Drupal.CTools.Modal.modal_display;
         });
 
       // Bind our custom event to the form submit
-      $('#modal-content form:not(.ctools-use-modal-processed)', context) 
+      $('#modal-content form:not(.ctools-use-modal-processed)', context)
         .addClass('ctools-use-modal-processed')
-        .each(function() { 
-
-          $(this).ajaxForm();
+        .each(function() {
+          $('input[type=submit], button', this).click(function() {
+            this.form.clk = this;
+          });
 
           var element_settings = {};
 
           element_settings.url = $(this).attr('action');
-          element_settings.setClick = true;
           element_settings.event = 'submit';
-          element_settings.progress = { 'type': 'throbber' } 
+          element_settings.progress = { 'type': 'throbber' }
           var base = $(this).attr('id');
 
           Drupal.ajax[base] = new Drupal.ajax(base, this, element_settings);
@@ -286,7 +286,7 @@
    * Display loading
    */
   //Drupal.CTools.AJAX.commands.modal_loading = function(command) {
-  Drupal.CTools.Modal.modal_loading = function(command) { 
+  Drupal.CTools.Modal.modal_loading = function(command) {
     Drupal.CTools.Modal.modal_display({
       output: Drupal.theme(Drupal.CTools.Modal.currentSettings.throbberTheme),
       title: Drupal.CTools.Modal.currentSettings.loadingText
